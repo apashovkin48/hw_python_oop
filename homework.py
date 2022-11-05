33d074d0a28638e3b6db2ea89666e3246a2f368e
Андрей Пашовкин <andre.pashovkin@yandex.ru>
2022-11-05 15:27:12 +0200

    remove annotation for list

--- homework.py
+++ homework.py
@@ -146,10 +146,7 @@ class Swimming(Training):
         )
 
 
-def read_package(
-    workout_type: str,
-    data: list[int, int, int, int, int]
-) -> Training:
+def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     if workout_type in 'SWM' or workout_type in 'RUN' or workout_type in 'WLK':
         training_names: dict[str, Training] = {
